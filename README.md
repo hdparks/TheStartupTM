868552253d8ce9648f4c9a11600044f02736ad5c
hparkinson <hparkinson@athlos.org>
2024-07-13 00:43:56 -0600

    done for the night

--- README.md
+++ README.md
@@ -12,10 +12,28 @@ As the founder of the worlds first bajillion-dollar startup, you must constantly
     - [x] hire window
     - [x] hire devs with d
     - [x] switch to "d" to open dev window, "h" to hire, "f" to fire, "b" to focus bugs, "n" to focus new features (sliding scale 0 - 10)
-    - [ ] implement bug - feature focus
+    - [x] implement bug - feature focus
+    - [x] implement cash cap
+    - [x] implement stock price loss condition
+    - [x] start card
+    - [x] end card
+    - [x] implement user loss per second per bug
+    - [ ] get rid of dev overlay
+    - [x] remap keys to be global - only help
+    - [ ] global help 
+    - [x] esc to pause, exit from there
+    - [ ] QA (q) to squash bugs for money, slows features
+    - [ ] Marketing (m) to increase adoption
+    - [ ] Business Strategist (s) to increase user pool
+    - [ ] Influencer (i) to increase user pool
 
+# Balance
 
 - [ ] make pretty
-    - [ ] tetris bar of total cash
+    - [x] tetris bar of total cash
+    - [ ] blinking red building when about to get crushed!
     - [ ] computer screen for features
     - [ ] computer screen for bugs
+
+# Backlot
+- [ ] Multiplayer w/ Wish
--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/muesli/reflow/ansi"
 )
 
 type model struct {
@@ -24,27 +28,60 @@ type model struct {
     
     progressTowardFeature int
     progressTowardBug int
+    progressTowardBugFix int
     progressTowardUser int
     progressTowardLostUser int
     copyPasteModifier int
 
-    devWindow bool
-    devHelp help.Model
+    height int
+    width int
+
+    helpWindow bool
+    helpModel help.Model
+
+    cashParticles [20]particle
+
+    devFocus int
+    devFocusProgress progress.Model
+    
+    gameTicking bool
+
+    scene GameScene
+    failureCause string
+
+    // debug
+    debug string
+}
+
+type GameScene int
+const (
+    Start GameScene = iota
+    Game
+    End
+)
+
+type particle struct {
+    x int
+    y int
 }
 
 var baseStyle = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
 var FEATURE_SECONDS_PER_USER = 60
+var BUG_SECONDS_PER_LOST_USER = 180
 var CASH_PER_SECOND_PER_USER_PER_FEATURE = 1
 var DEV_FEATURE_SECONDS_PER_BUG = 200
 var DEV_SECONDS_PER_FEATURE = 60
+var DEV_SALARY_PER_SECOND = 1
 var PRICE_PER_FEATURE = 100
 var PRICE_PER_USER = 100
 var PRICE_PER_BUG = -500
 var PRICE_PER_DEV = 1000
 var PRICE_PER_MARKETER = 1000
+var CASH_CAP = 1000000
+
 
 func initialModel() model {
-    return model {
+        return model {
         pricePerShare: 0,
         cash: 0,
         users: 1,
@@ -55,34 +92,55 @@ func initialModel() model {
 
         progressTowardFeature:0,
         progressTowardBug:0,
+        progressTowardBugFix:0,
         progressTowardUser:0,
         progressTowardLostUser: 0,
         copyPasteModifier: 0,
 
-        devWindow: false,
-        devHelp: help.New(),
+        height: 0,
+        width: 0,
+
+        helpWindow: false,
+        helpModel: help.New(),
+
+        cashParticles: [20]particle{},
+
+        devFocus: 10,
+        devFocusProgress: progress.New(progress.WithSolidFill("4"), progress.WithWidth(10)),
+
+        scene: Start,
+        gameTicking: true,
     }
 }
 
-type TickMsg time.Time
 
 func (m model) Init() tea.Cmd {
-    return tea.Batch(doTick(), textinput.Blink)
+    return tea.Batch(doGameTick(), doFrameTick(), textinput.Blink)
 }
 
-func doTick() tea.Cmd {
+type GameTickMsg time.Time
+
+func doGameTick() tea.Cmd {
     tick := tea.Tick(time.Second, func(t time.Time) tea.Msg {
-        return TickMsg(t)
+        return GameTickMsg(t)
     })
     return tick
 }
 
-func onTick(m model) model { 
+func onGameTick(m model) model { 
+    if (!m.gameTicking){
+        return m
+    }
 
-    m.progressTowardFeature += m.devs
+    m.progressTowardFeature += (m.devs * m.devFocus) / 10
     newFeatures := m.progressTowardFeature / DEV_SECONDS_PER_FEATURE
     m.features += newFeatures
-    m.progressTowardFeature -= newFeatures * DEV_SECONDS_PER_FEATURE
+    m.progressTowardFeature -= newFeatures * DEV_SECONDS_PER_FEATURE 
+
+    m.progressTowardBugFix += (m.devs * (10 - m.devFocus)) / 10
+    bugFixes := m.progressTowardBugFix / DEV_SECONDS_PER_FEATURE
+    m.bugs -= bugFixes
+    m.progressTowardBugFix -= bugFixes * DEV_SECONDS_PER_FEATURE
 
     m.progressTowardBug += m.devs * m.features
     newBugs := m.progressTowardBug / DEV_FEATURE_SECONDS_PER_BUG
@@ -93,6 +151,12 @@ func onTick(m model) model {
     newUsers := m.progressTowardUser / FEATURE_SECONDS_PER_USER
     m.users += newUsers
     m.progressTowardUser -= newUsers * FEATURE_SECONDS_PER_USER
+
+    m.progressTowardLostUser += m.bugs
+    lostUsers := m.progressTowardLostUser / BUG_SECONDS_PER_LOST_USER 
+    m.users -= lostUsers
+    m.progressTowardUser -= lostUsers * BUG_SECONDS_PER_LOST_USER 
+
     
     m.cash += CASH_PER_SECOND_PER_USER_PER_FEATURE * m.users * m.features
 
@@ -101,52 +165,64 @@ func onTick(m model) model {
                         PRICE_PER_BUG * m.bugs +
                         PRICE_PER_USER * m.users +
                         PRICE_PER_MARKETER * m.marketers
-         
+        
+    if(m.cash > CASH_CAP){
+        m.scene = End
+        m.failureCause = "You've been crushed under the weight of your own success...\nA tragedy has befallen all mankind."
+    }
+
+    if(m.pricePerShare < 0) {
+        m.scene = End
+        m.failureCause = "Your enterprise has colapsed around you. A flash in the pan, nothing more."
+    }
+
     return m
 }
 
-func(m model) UpdateMain(msg tea.KeyMsg) model {
-    switch msg.String() {
-    
-    case "{", "}","[","]","j","k":
-        m = m.ApplyProgressTowardFeature(1)
 
-    case "ctrl+c", "ctrl-v":
-        m.copyPasteModifier = min(m.copyPasteModifier+1,10)
 
-    case "1","2":
-        m.bugs = max(0, m.bugs - 1)
 
-    case "d":
-        m.devWindow = !m.devWindow
+type FrameTickMsg time.Time
+func doFrameTick() tea.Cmd {
+    tick := tea.Tick(time.Second/12, func(t time.Time) tea.Msg {
+        return FrameTickMsg(t)
+    })
+    return tick
+}
+
+func onFrameTick(m model) model {
+    for i:=0;i<len(m.cashParticles);i++{
+        d20 := rand.Intn(20)
+        x := m.cashParticles[i].x
+        if (d20 < x - 1){ m.cashParticles[i].x -= 1 }
+        if (d20 > x + 1){ m.cashParticles[i].x += 1 }
+        m.cashParticles[i].y = (m.cashParticles[i].y + rand.Intn(2)) % 20
     }
     return m
 }
 
+
 type devKeyMap struct {
-    Close key.Binding
     Hire key.Binding
     Fire key.Binding
     FocusBugs key.Binding
     FocusNewFeatures key.Binding
     Help key.Binding
+    Features key.Binding
+    Bugs key.Binding
 }
 
 func (k devKeyMap) ShortHelp() []key.Binding {
-    return []key.Binding{k.Help, k.Close}
+    return []key.Binding{k.Help}
 }
 func (k devKeyMap) FullHelp() [][]key.Binding {
     return [][]key.Binding{
         {k.Hire, k.Fire, k.FocusBugs, k.FocusNewFeatures},
-        {k.Help, k.Close},
+        {k.Help},
     }
 }
 
 var devKeys = devKeyMap{
-    Close: key.NewBinding(
-        key.WithKeys("d"),
-        key.WithHelp("d","close menu"),
-    ),
     Hire: key.NewBinding(
         key.WithKeys("h"),
         key.WithHelp("h","hire dev"),
@@ -163,6 +239,14 @@ var devKeys = devKeyMap{
         key.WithKeys("n"),
         key.WithHelp("n","focus new features"),
     ),
+    Features: key.NewBinding(
+        key.WithKeys("{", "}","[","]","j","k"),
+        key.WithHelp("jkl;","make features"),
+    ),
+    Bugs: key.NewBinding(
+        key.WithKeys("1","2","3","4"),
+        key.WithHelp("1234","fix bugs"),
+    ),
     Help: key.NewBinding(
         key.WithKeys("?"),
         key.WithHelp("?", "help"),
@@ -170,53 +254,108 @@ var devKeys = devKeyMap{
 }
 
 
-func (m model) UpdateDev(msg tea.KeyMsg) model {
-    switch {
-
-    case key.Matches(msg,devKeys.Close):
-        m.devWindow= !m.devWindow
-
-    case key.Matches(msg, devKeys.Hire):
-        m.devs += 1
-
-    case key.Matches(msg, devKeys.Fire):
-        m.devs -= 1
-  
-    case key.Matches(msg, devKeys.Help):
-        m.devHelp.ShowAll = !m.devHelp.ShowAll
-
-    }
-    return m    
-}
-
 func(m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
     case tea.WindowSizeMsg:
-        m.devHelp.Width = msg.Width
+        m.width = min(200,msg.Width)
+        m.height = min(20,msg.Height)
+        m.helpModel.Width = msg.Width
 
     case tea.KeyMsg: 
-        if msg.String() == tea.KeyEsc.String() {
-            fmt.Println("quitting")
+        m.debug = msg.String()
+
+        if msg.String() == tea.KeyEsc.String(){
             return m, tea.Quit;
         }
 
-        if (m.devWindow) {
-            m = m.UpdateDev(msg)
-        } else {
-            m = m.UpdateMain(msg)
+        if (m.scene == Start){
+            m.scene = Game
         }
 
-    case TickMsg:
-        m = onTick(m)
-        return m, doTick()
+        switch {
 
+        case key.Matches(msg, devKeys.Hire):
+            m.devs += 1
+
+        case key.Matches(msg, devKeys.Fire):
+            m.devs -= 1
+
+        case key.Matches(msg, devKeys.FocusBugs):
+            m.devFocus = max(0, m.devFocus - 1)
+
+        case key.Matches(msg, devKeys.FocusNewFeatures):
+            m.devFocus = min(10, m.devFocus + 1)
+      
+        case key.Matches(msg, devKeys.Help):
+            m.helpWindow = !m.helpWindow
+
+        case key.Matches(msg, devKeys.Features):
+            m = m.ApplyProgressTowardFeature(1)
+
+        case key.Matches(msg, devKeys.Bugs):
+            m.bugs = max(0, m.bugs - 1)
+        }
+
+
+    case GameTickMsg:
+        m = onGameTick(m)
+        return m, doGameTick()
+
+    case FrameTickMsg:
+        m = onFrameTick(m)
+        return m, doFrameTick()
     }
 
     return m, nil 
 }
 
+var cashStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("35"))
+var startupStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("7"))
+var cashParticleRunes = []rune{'◜','\'',',','◝','◃','"','◟','◞'}
+func randomRune() rune {
+    return cashParticleRunes[rand.Intn(len(cashParticleRunes))]
+}
+
+
+
+func (m model) CashWindow() string {
+
+    cashTube := "   |           |\n"
+    cashTube += "  .─────────────.\n"
+    cashTube += " /    Cash       \\\n"
+    cashTube += "/.───────────────.\\\n"
+    cashTube += "(                 )\n"
+    cashTube += " `───────────────' \n"
+    cashTube += strings.Repeat("\n", max(0,m.height - 14)) 
+
+
+    cashTube = cashStyle.Render(cashTube)
+
+
+    startupBuilding := "   ┌──────────┐   \n"
+    startupBuilding += "┌─┬┴──────────┴┬─┐\n"
+    startupBuilding += "│ │  STaRtupTM │ │\n"
+    startupBuilding += "│ └────────────┘ │\n"
+    startupBuilding += "│◫ ◫ ◫ ◫  ◫ ◫ ◫ ◫│\n"
+    startupBuilding += "│◫ ◫ ◫ ◫  ◫ ◫ ◫ ◫│\n"
+    startupBuilding += "└──────┮◚◚┭──────┘\n"
+    startupBuilding = startupStyle.Render(startupBuilding)
+
+    s := cashTube + "\n" + startupBuilding
+
+    l := len(CashLevels)
+    cashSize := min(m.cash / 1000, l-1)
+    cashPile := CashLevels[cashSize]
+    s = PlaceOverlay(0+cashPile.x, 5+cashPile.y, cashStyle.Render(cashPile.view), s, false)
+
+    for i:=0;i<len(m.cashParticles);i++ {
+        s = PlaceOverlay(m.cashParticles[i].x, 3 + m.cashParticles[i].y, cashStyle.Render(string(randomRune())), s, false)
+    }
+
+    return s 
+}
+
 func (m model) TableView() string {
-    s := "The Startup:tm:\n\n"
    
     cols := []table.Column{
         {Title: "", Width: 16},
@@ -238,30 +377,83 @@ func (m model) TableView() string {
         table.WithColumns(cols),
     )
 
-    s += baseStyle.Render(t.View()) + "\n"
-
-    return s
+    return t.View()
 }
 
 
 var devBorder = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("63"));
 
-func (m model) DevWindow() string {
-    
-    dev := fmt.Sprintf("Hire 1 dev\n%v\n", m.devHelp.View(devKeys)) 
+func (m model) DevWindowView() string {
+    dev := m.helpModel.FullHelpView(devKeys.FullHelp())
     return devBorder.Render(dev)
 }
 
+func maxWidth(s []string) int {
+    maxW := 0
+    for i:=0; i<len(s); i++ {
+        maxW = max(ansi.PrintableRuneWidth(s[i]),maxW)
+    }
+    return maxW
+}
 
 func (m model) View() string {
-    
-    base := m.TableView()
-
-    if (m.devWindow) {
-        devOverlay := m.DevWindow()
-        base = PlaceOverlay(10,5, devOverlay, base, true)
+    switch(m.scene) {
+    case Start:
+        return m.StartView()
+    case Game:
+        return m.GameView()
+    case End:
+        return m.EndView()
     }
+    return "State not found"
+}
+
+func baseScreenStyle(m model) lipgloss.Style {
+    style := baseStyle.Width(min(m.width,200)).Height(min(m.height,20)).UnsetAlign()
+    return style
+}
+
+
+func (m model) GameView() string {
+    style := baseScreenStyle(m)
+    m.debug = fmt.Sprintf("%v",style.GetAlign())
+    base := style.Render(m.TableView())
+
+    cashView := m.CashWindow()
+    
+    w := maxWidth(strings.Split(cashView, "\n"))
+    base = PlaceOverlay(m.width-w-1, 1, cashView, base, false)
+
+    if (m.helpWindow) {
+        devOverlay := m.DevWindowView()
+        lines := strings.Split(devOverlay, "\n")
+        width := maxWidth(lines)
+        base = PlaceOverlay(m.width/2 - width/2, m.height/2 - len(lines)/2, devOverlay, base, true)
+    }
+
+    shortHelp := m.helpModel.ShortHelpView(devKeys.ShortHelp())
+    base += "\n" + shortHelp + "\n"
+
+    base += "\n" + m.debug + "\n"
     return base 
+}
+
+func (m model) StartView() string {
+    style := baseScreenStyle(m)
+    return style.Align(lipgloss.Center, lipgloss.Center).Render(`
+████████╗██╗  ██╗███████╗    ███████╗████████╗ █████╗ ██████╗ ████████╗██╗   ██╗██████╗      ██╗████████╗███╗   ███╗██╗ 
+╚══██╔══╝██║  ██║██╔════╝    ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██╔══██╗    ██╔╝╚══██╔══╝████╗ ████║╚██╗
+   ██║   ███████║█████╗      ███████╗   ██║   ███████║██████╔╝   ██║   ██║   ██║██████╔╝    ██║    ██║   ██╔████╔██║ ██║
+   ██║   ██╔══██║██╔══╝      ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██║   ██║██╔═══╝     ██║    ██║   ██║╚██╔╝██║ ██║
+   ██║   ██║  ██║███████╗    ███████║   ██║   ██║  ██║██║  ██║   ██║   ╚██████╔╝██║         ╚██╗   ██║   ██║ ╚═╝ ██║██╔╝
+   ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝          ╚═╝   ╚═╝   ╚═╝     ╚═╝╚═╝ 
+`)
+}
+
+func (m model) EndView() string {
+    style := baseStyle
+    base := style.Width(min(m.width,200)).Height(min(m.height,20)).Align(lipgloss.Center,lipgloss.Center).Render(m.failureCause)
+    return base
 }
 
 func main() {
